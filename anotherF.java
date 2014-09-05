ee016fb88f6307c68c9fe73948b8dec4074c60ea
Qi Gao <gaoq@umich.edu>
2014-09-05 17:04:31 -0400

    created a new branch and made some changes

---
+++ anotherF.java
@@ -0,0 +1,5 @@
+public class anotherF {
+	void toString (String msg) {
+		System.out.println("This is inside another function; message received is: " + msg);
+	}
+}
--- testFunction.java
+++ testFunction.java
@@ -3,6 +3,8 @@ public class testFunction {
 	void displayFunction (String msg) {
 	System.out.println("Message I received is: " + msg);
 	System.out.println("I have done with modification! New message is: " + msg + " modified");
-}
+	anotherF f = new anotherF();
+	f.toString("hello world again");
+	}
 
 }
